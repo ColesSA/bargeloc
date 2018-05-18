efd7bf2a533b16a662224be36d70576194aa22c1
ColesSA <ColesSA@cmog.org>
2018-05-18 11:49:37 -0400

    Database Connection
    Completed database connection and table entry. Next step is to make the code more usable and formatted correctly.

--- main.py
+++ main.py
@@ -1,51 +1,48 @@
+import datetime
 import json
-import requests
+import time
+from urllib.parse import quote_plus
+
 import dataset
-import time, datetime
-import pyodbc
-import urllib.parse
+import requests
 from bs4 import BeautifulSoup
-from sqlalchemy import create_engine
+
+import pyodbc
 
 with open("./config.json", 'r') as stream:
         config = json.load(stream)
 
- # This section will be changed later to something less crap 
- # once it works and I do some learning on python lists.
+# This section will be changed later to something less crap 
+# once it works and I do some learning on python lists.
 
- # Populate local vars with config data 
+# Populate local vars with config data 
 web_url = config['WEB']['URL']
 web_user = config['WEB']['UID']
 web_pass = config['WEB']['PWD']
 
-db_driver = config['DATABASE']['DRIVER']
 db_server = config['DATABASE']['SERVER']
 db_database = config['DATABASE']['DB']
 db_user = config['DATABASE']['UID']
 db_pass = config['DATABASE']['PWD']
-db_app = config['DATABASE']['APP']
 
-db_dialect = config['test']['dialect']
-
- # get the time retrieved
-time = datetime.datetime.fromtimestamp(time.time()).strftime('%Y-%m-%d %H:%M:%S')
-
- # get the page data
+# get the page data
 page = requests.get(web_url, verify=False, auth=(web_user, web_pass))
 page_data = BeautifulSoup(page.content, 'html.parser')
 
- # populate latitude and longitude vars with values
+# populate latitude and longitude vars with values
 lat = page_data.find('div',{'id' : 'latlong'})['data-latitude']
 lon = page_data.find('div',{'id' : 'latlong'})['data-longitude']
 
- # make list, because
-latlon = {'timestamp' : time, 'latitude' : lat, 'longitude' : lon}
+# initialize the database as a dataset object
+db = dataset.connect('mssql+pyodbc:///?odbc_connect={}'.format(quote_plus('DRIVER={ODBC Driver 13 for SQL Server};SERVER='+db_server+';DATABASE='+db_database+';UID='+db_user+';PWD='+db_pass)))
 
-#print(latlon)
+# get the current time to use as time retrieved
+time = datetime.datetime.fromtimestamp(time.time()).strftime('%Y-%m-%d %H:%M:%S')
 
- # initialize the database as an object
-#print('{}+pyodbc://{}:{}@{}/{}?driver={}'.format(db_dialect,db_user,db_pass,db_server,db_database,db_driver))
-db = dataset.connect('{}+pyodbc://{}:{}@{}/{}?driver={}'.format(db_dialect,db_user,db_pass,db_server,db_database,db_driver))
+# write test table to database
+table = db['testTable']
+table.insert({'timestamp' : time, 'latitude' : lat, 'longitude' : lon})
 
-table = db['firstTest']
-table.insert(latlon)
+# print table to show success
+for entry in db['testTable']:
+        print(entry)
